6197eb18c45a041311654dc63a68845c5bc61c99
Dulaj Kulathunga <dulajworks@gmail.com>
2025-06-13 00:28:53 +0800

    Initial maven project setup

---
+++ scratch-game/src/main/java/com/dulaj/scratchgame/App.java
@@ -0,0 +1,13 @@
+package com.dulaj.scratchgame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
